1b7820a9e614b039aaeba4603c49782abd702fe1
HCDani <hockdanield@gmail.com>
2024-10-10 12:58:20 +0200

    example code for bottom tab navbar

--- app/index.tsx
+++ app/index.tsx
@@ -1,50 +1,54 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-
 function HomeScreen() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
-    );
-}
-function HospitalScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Hospital Screen</Text>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home screen</Text>
         </View>
     );
 }
 
-const Stack = createNativeStackNavigator();
-
-function App() {
+function SettingsScreen() {
     return (
-
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings screen</Text>
+        </View>
     );
 }
+
 
 const Tab = createBottomTabNavigator();
 
-function Tabs() {
-    <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={HospitalScreen} />
-        <Tab.Navigator
-            backBehavior={history}
-        />
-    </Tab.Navigator>
+export default function App() {
+    return (
+        <Tab.Navigator tabBar={props => <TabBar {...props} />} backBehavior="history" screenOptions={{ headerShown: false }}>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
 }
-
-export default App;
+function TabBar({ navigation }) {
+    return (
+        <View>
+        <Button
+            title="Home"
+            onPress={() => {
+                // Navigate using the navigation prop that you received
+                navigation.navigate('Home');
+            }}
+        />
+         <Button
+            title="Back"
+            onPress={() => {
+                //Navigate back to the previous screen.
+                navigation.goBack();
+            }}
+        />
+       </View>
+    );
+}
