e09d5452da9468a619e4be6cb4b2bf3fe487d846
JustAugucis <augustinaskazl@gmail.com>
2025-01-09 14:06:06 +0100

    new main screen and profesionals screen

---
+++ components/screens/main.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import {  Text, View, StyleSheet } from 'react-native';
+import CustomButton from '@/components/Button';
+import { Colors } from '@/constants/Colors';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+type RootStackParamList = {
+    WebView: { url: string };
+};
+
+type AppProps = {
+    navigation: NativeStackNavigationProp<RootStackParamList, 'WebView'>;
+  
+};
+
+const URL_BASE = "https://delirium.hock.hu/";
+
+  export default function App({navigation}: AppProps) {
+    return (
+      <View style={styles.container}>
+          <View>
+              {/* Professional Staff Button */}
+              <CustomButton
+                  label={
+                      <View style={styles.buttonContent}>
+                          <Icon name="work" size={20} color="white" style={styles.icon} />
+                          <Text style={styles.buttonText}>Professional Staff</Text>
+                      </View>
+                  }
+                  color="#57a40a"
+                  style={styles.customButton}
+                  onPress={() =>
+                      navigation.navigate('Profesional Staff')
+                  }
+              />
+              {/* Relatives Button */}
+              <CustomButton
+                  label={
+                      <View style={styles.buttonContent}>
+                          <Icon name="family-restroom" size={20} color="white" style={styles.icon} />
+                          <Text style={styles.buttonText}>Relatives</Text>
+                      </View>
+                  }
+                  color="#8b0000"
+                  style={styles.customButton}
+                  onPress={() =>
+                      navigation.navigate('WebView', {
+                          url: URL_BASE + 'relatives', // Pass URL to WebView
+                      })
+                  }
+              />
+          </View>
+    </View>
+        
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.light.background, 
+    },
+    topButtonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly', 
+        alignItems: 'center',
+        width: '90%', 
+        marginVertical: 15, 
+    },
+    buttonContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    icon: {
+        marginRight: 6, 
+    },
+    buttonText: {
+        fontSize: 17,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    customButton: {
+        flex:1,
+        maxWidth: '45%', 
+        paddingVertical: 10, 
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 1, 
+        borderWidth: 1,
+        borderColor: '#000',
+        borderRadius: 0,
+        backgroundColor: 'transparent',
+        
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: Colors.light.text, 
+        marginBottom: 20,
+    },
+});
